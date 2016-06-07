40afbd327b0ac4f70d31e0244d016b132570f19a
breakit(백승용) <breakit@coupang.com>
2016-06-07 12:53:02 +0900

    연습문제 풀이
     - test code 수정

--- breakit/04/replacefun_test.go
+++ breakit/04/replacefun_test.go
@@ -3,10 +3,9 @@ package replacefun
 import (
 	"strings"
 	"fmt"
-	"testing"
 )
 
-func TestEvalReplace(t *testing.T) {
+func ExampleEvalReplace() {
 	in := strings.Join([]string{
 		"다들 그 동안 고생이 많았다.",
 		"첫째는 분당에 있는 { 2 ** 4 * 3 }평 아파트를 갖거라.",
@@ -18,6 +17,10 @@ func TestEvalReplace(t *testing.T) {
 
 	fmt.Println(EvalReplaceAll(in))
 	// Output:
-	// 다들 그 동안 고생이 많았다.\n첫째는 분당에 있는 48평 아파트를 갖거라.\n둘째는 임야 6676평을 가져라.\n막내는 3324평 임야를 갖고.\n배기량 632cc의 경운기를 갖거라.
+	// 다들 그 동안 고생이 많았다.
+	// 첫째는 분당에 있는 48평 아파트를 갖거라.
+	// 둘째는 임야 6676평을 가져라.
+	// 막내는 3324평 임야를 갖고.
+	// 배기량 632cc의 경운기를 갖거라.
 
 }
